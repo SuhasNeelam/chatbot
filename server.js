959bd3fa9cb2f289571e25baff802ec53dda1b60
Suhas Neelam <suhasneelam38@gmail.com>
2021-01-29 16:50:26 +0530

    Added Issues

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const { WebhookClient } = require("dialogflow-fulfillment");
+const { WebhookClient, Payload } = require("dialogflow-fulfillment");
 const Number = require('./number')
 
 mongoose.connect('mongodb://localhost:27017/numbers', { useNewUrlParser: true,useUnifiedTopology: true })
@@ -20,13 +20,15 @@ app.use('/numbers', numbersRouter)
 app.listen(3000, () => console.log('server started'))
 
 let username = ""
+let issueDict
 
 app.post("/", express.json(), (req, res) => {
     const agent = new WebhookClient({ request: req, response: res })
     let intentMap = new Map()
+
     intentMap.set("ComplaintServiceNumber", (agent) => {
-      let number = agent.parameters['phone-number'];
-      let url = `http://da85b235ba31.ngrok.io/numbers/${number}`;
+      let number = agent.parameters['phone-number']
+      let url = `https://0e073582366c.ngrok.io/numbers/${number}`
       function getUsername(url){
         const axios = require('axios')
         return axios.get(url)
@@ -44,24 +46,90 @@ app.post("/", express.json(), (req, res) => {
         bot_res = "Sorry, No user with the phone number. Thank You!"
         agent.end(bot_res)
       })
-
-
     })
+
     intentMap.set("ComplaintServiceNumberTicket", (agent) => {
-      let issue = agent.parameters["any"]
+      let Buffering = agent.parameters['Buffering']
+      let Internet = agent.parameters['Internet']
+      let Down = agent.parameters['Down']
+      let DNS = agent.parameters['DNS']
+      if(DNS || Down || Buffering || Internet){
+        let payloadData = {
+          "richContent": [
+            [
+              {
+                "type": "list",
+                "title": "Internet Down",
+                "subtitle": "Press '1' for Internet is down",
+                "event": {
+                  "parameters": {},
+                  "languageCode": "",
+                  "name": ""
+                }
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "event": {
+                  "name": "",
+                  "languageCode": "",
+                  "parameters": {}
+                },
+                "title": "Buffering",
+                "type": "list",
+                "subtitle": "Press '2' Buffering"
+              },
+              {
+                "type": "divider"
+              },
+              {
+                "event": {
+                  "languageCode": "",
+                  "name": "",
+                  "parameters": {}
+                },
+                "subtitle": "Press '3' for DNS problem",
+                "type": "list",
+                "title": "DNS problem"
+              }
+            ]
+          ]
+        }
+      issueDict = {
+        1 : "Internet Down",
+        2 :  "Buffering",
+        3 :  "DNS problem"
+      }
+      agent.add( new Payload(agent.UNSPECIFIED, payloadData, {sendAsMessage: true, rawPayload: true }))
+      }
+      else{
+        agent.end("We don't do that here!")
+      }
+    })
+
+    intentMap.set("ComplaintServiceNumberTicketConfirm", (agent) => {
+      let issue = agent.parameters["number"]
+      console.log(issue)
+      console.log(issueDict)
+      if(issue>3 || issue<1){
+        agent.add("Please enter a valid choice!")
+        return
+      }
       var ID = function () {
-        return '_' + Math.random().toString(36).substr(2, 9);
-      };
+        return '_' + Math.random().toString(36).substr(2, 9)
+      }
       let bot_res = "default res"
         const tNo = ID()
-        bot_res = `${username}, your complaint has been registered! Ticket number for future reference is ${tNo}. Thank you for your time!`
+        bot_res = `${username}, your complaint has been registered on ${issueDict[issue]}! Ticket number for future reference is ${tNo}. Thank you for your time!`
         agent.end(bot_res)
         return Number.updateOne(
 	      {username: username},
-	      { $set: { ticketNo: tNo, complaintStatus: "Pending", issue: issue}}
+	      { $set: { ticketNo: tNo, complaintStatus: "Pending", issue: issueDict[issue]}}
         ).then(ref =>
         console.log("Ticket details added to DB")
         )
     })
+
     agent.handleRequest(intentMap)
 }) 
